b3d1b5ba4683b0cdd9b4e3df5189ca6b2250f48f
ficserbiYY <ficserbiy@gmail.com>
2026-07-12 13:18:30 +0500

    chore: initialize the Enviroment settings

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-secure-secret_key'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+load_dotenv()
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
