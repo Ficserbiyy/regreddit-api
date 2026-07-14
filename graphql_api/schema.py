94747b57de04bf18a96110b3b16ae261a03a08c7
ficserbiYY <ficserbiy@gmail.com>
2026-07-15 00:25:57 +0500

    feat: expose posts and communities relationships through GraphQL

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,6 +1,7 @@
 import strawberry
 from communities.models import Community
-from .types import CommunityType
+from posts.models import Post
+from .types import CommunityType, PostType
 
 
 @strawberry.type
@@ -9,6 +10,9 @@ class Query:
     @strawberry.field
     def communities(self) -> list[CommunityType]:
         return Community.objects.all()  # type: ignore
-
+    
+    @strawberry.field
+    def posts(self) -> list[PostType]:
+        return Post.objects.all()       # type: ignore
 
 schema = strawberry.Schema(query=Query)
--- graphql_api/types.py
+++ graphql_api/types.py
@@ -1,5 +1,20 @@
 import strawberry.django
 from communities.models import Community
+from users.models import User
+from posts.models import Post
+
+
+@strawberry.django.type(User)
+class UserType:
+    id: strawberry.auto
+    username: strawberry.auto
+
+
+@strawberry.django.type(Post)
+class PostType:
+    id: strawberry.auto
+    title: strawberry.auto
+    creator: UserType
 
 
 @strawberry.django.type(Community)
@@ -7,3 +22,5 @@ class CommunityType:
     id: strawberry.auto
     name: strawberry.auto
     description: strawberry.auto
+    creator: UserType
+    posts: list[PostType]
