3ef9fb279e914b7061368cc418035d8a6a55f2d4
ficserbiYY <ficserbiy@gmail.com>
2026-07-15 16:06:23 +0500

    feat: add GraphQL queries for fetching a single community

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,4 +1,5 @@
 import strawberry
+from django.shortcuts import get_object_or_404
 from communities.models import Community
 from posts.models import Post
 from .types import CommunityType, PostType
@@ -9,10 +10,25 @@ class Query:
 
     @strawberry.field
     def communities(self) -> list[CommunityType]:
-        return Community.objects.all()  # type: ignore
-    
+        ''' List all existing communities '''
+        return Community.objects.all()                         # type: ignore
+
+    @strawberry.field
+    def community(
+        self,
+        id: int | None = None,
+        name: str | None = None,
+    ) -> CommunityType:
+
+        if id:
+            return get_object_or_404(Community, pk=id)         # type: ignore
+        if name:
+            return get_object_or_404(Community, name=name)     # type: ignore
+
+        raise ValueError("Either 'id' or 'name' must be provided.")
+
     @strawberry.field
     def posts(self) -> list[PostType]:
-        return Post.objects.all()       # type: ignore
+        return Post.objects.all()                              # type: ignore
 
 schema = strawberry.Schema(query=Query)
